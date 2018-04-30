1f8d2f876f3660854e8bc78e2f1ffb65f5c45b07
man <man.movin@gmail.com>
2018-04-30 10:52:42 -0300

    Version inicial conexion BLE

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -1,24 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+import { ToastController } from 'ionic-angular';
+import { PerfilesPage } from '../perfiles/perfiles';
 
-/**
- * Generated class for the InicioPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-inicio',
   templateUrl: 'inicio.html',
 })
 export class InicioPage {
+  devices: any[] = [];
+  statusMessage: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public toastCtrl: ToastController,
+    public ngZone: NgZone,
+    public ble: BLE
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('Inicio pagina cargada');
   }
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  scan() {
+    this.setStatus('Escaneando dispositivos Bluetooth LE');
+    this.devices = [];  // clear list
+    this.ble.scan([], 5).subscribe(
+      device => this.onDeviceDiscovered(device), 
+      error => this.scanError(error)
+    );
+
+    setTimeout(this.setStatus.bind(this), 5000, 'Escaneo completo');
+  }
+
+  onDeviceDiscovered(device) {
+    console.log('Reconocido: ' + JSON.stringify(device, null, 2));
+    this.mostrarToast('Reconocido: ' + JSON.stringify(device, null, 2));
+    this.ngZone.run(() => {
+      this.devices.push(device);
+    });
+  }
+  mostrarToast(msg:string){
+    let toast = this.toastCtrl.create({
+      message: msg,
+      position: 'middle',
+      duration: 5000
+    });
+    toast.present();
+  }
+  // If location permission is denied, you'll end up here
+  scanError(error) {
+    this.setStatus('Error ' + error);
+    this.mostrarToast('Error scaneando dispositivos Bluetooth low energy');
+  }
+
+  setStatus(message) {
+    console.log(message);
+    this.ngZone.run(() => {
+      this.statusMessage = message;
+    });
+  }
+
+  deviceSelected(device) {
+    console.log(JSON.stringify(device) + ' selected');
+    this.navCtrl.push(PerfilesPage, {
+      device: device
+    });
+  }
+
+
 
 }
--- src/pages/perfiles/perfiles.ts
+++ src/pages/perfiles/perfiles.ts
@@ -1,24 +1,67 @@
-import { Component } from '@angular/core';
+import { Component , NgZone } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PerfilesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ToastController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
 
 @Component({
   selector: 'page-perfiles',
   templateUrl: 'perfiles.html',
 })
 export class PerfilesPage {
+  peripheral: any = {};
+  statusMessage: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public ble: BLE,
+    public toastCtrl: ToastController,
+    public ngZone: NgZone
+  ) {
+     let device = navParams.get('device');
+    if (device){
+      this.setStatus('Conectando a ' + device.name || device.id);
+
+      this.ble.connect(device.id).subscribe(
+        peripheral => this.onConnected(peripheral),
+        peripheral => this.onDeviceDisconnected(peripheral)
+      );
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilesPage');
   }
+  
+  onConnected(peripheral) {
+    this.ngZone.run(() => {
+      this.setStatus('Conectado');
+      this.peripheral = peripheral;
+    });
+  }
 
+  onDeviceDisconnected(peripheral) {
+    let toast = this.toastCtrl.create({
+      message: 'El periferico se ha desconectado',
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
+  }
+
+  // Disconnect peripheral when leaving the page
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave disconnecting Bluetooth');
+    this.ble.disconnect(this.peripheral.id).then(
+      () => console.log('Disconnected ' + JSON.stringify(this.peripheral)),
+      () => console.log('ERROR disconnecting ' + JSON.stringify(this.peripheral))
+    )
+  }
+
+  setStatus(message) {
+    console.log(message);
+    this.ngZone.run(() => {
+      this.statusMessage = message;
+    });
+  }
 }
